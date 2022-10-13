d81efffdc3bc2ed701d3f9e8487c9de370c33e6d
Yuval Vaknin Levi <yuvallevi0950@gmail.com>
2022-10-13 15:19:30 +0300

    connect to db

--- BulkyBook/Data/ApplicationDbContext.cs
+++ BulkyBook/Data/ApplicationDbContext.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyBook.Data
 {
     public class ApplicationDbContext :DbContext
     {
-        public ApplicationDbContext()
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
