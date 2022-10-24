672d3ac1fca0431d6e4fbb167489a8e92b9a7c01
Yuval Vaknin Levi <yuvallevi0950@gmail.com>
2022-10-24 12:48:44 +0300

    custuom errors

--- BulkyBook/Controllers/CategoryController.cs
+++ BulkyBook/Controllers/CategoryController.cs
@@ -28,6 +28,10 @@ namespace BulkyBook.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match tha name");
+            }
             if (ModelState.IsValid)
             {
             _db.Categories.Add(obj);
