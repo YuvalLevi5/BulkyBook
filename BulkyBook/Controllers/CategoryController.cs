97bb05c15809f7be2d9b1fec18943a408f1fe3f2
Yuval Vaknin Levi <yuvallevi0950@gmail.com>
2022-10-25 17:00:58 +0300

    edit setup

--- BulkyBook/Controllers/CategoryController.cs
+++ BulkyBook/Controllers/CategoryController.cs
@@ -17,6 +17,7 @@ namespace BulkyBook.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         } 
+
         // GET
         public IActionResult Create()
         {
@@ -40,5 +41,37 @@ namespace BulkyBook.Controllers
             }
             return View(obj);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match tha name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
     }
 }
